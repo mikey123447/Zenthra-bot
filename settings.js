3b911783721eab295dcfcf5c6447485208990efd
mikey123447 <jesuslife509@gmail.com>
2025-07-11 17:42:55 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,9 +3,9 @@ const settings = {
   author: '‎',
   botName: "Zenthra-MD",
   botOwner: '𝙼𝚊𝚕𝚟𝚒𝚗 𝙺𝚒𝚗𝚐', // Your name
-  ownerNumber: '263776388689', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '50947629611', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "0.0.1",
   menuImageUrl: "https://files.catbox.moe/rjrgn3.jpg",
